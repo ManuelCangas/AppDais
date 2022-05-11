2df3727d4e7f12efcc9251a8fc429294f481f520
Vanesapisv <vanesa.yen.m@gmail.com>
2022-05-11 16:36:43 -0400

    Login, menú lateral sin toggle y bottom-menu

--- app/src/main/java/com/example/proyecto_daise/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_daise/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.proyecto_daise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val user_name=findViewById<EditText>(R.id.user_name)
+        val user_pass=findViewById<EditText>(R.id.user_pass)
+        val new_pass=findViewById<TextView>(R.id.new_pass)
+        val sign_in=findViewById<Button>(R.id.log_in)
+        //AQUI VA EL INICIAR SESION//
+        sign_in.setOnClickListener {
+            val log_in = Intent(this, Home::class.java)
+            startActivity(log_in)
+        }
     }
 }
